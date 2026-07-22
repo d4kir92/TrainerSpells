94184a8bb60717ce66e8b6b40a37cafc8b7c0806
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-07-22 20:00:53 +0200

    refactor leatherworking

--- data/classicera/LeatherworkingRecipes.lua
+++ data/classicera/LeatherworkingRecipes.lua
@@ -1,9 +1,47 @@
 TrainerSpellsBuiltin_ProfessionRecipe = TrainerSpellsBuiltin_ProfessionRecipe or {}
 TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking = TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking or {
+    [90] = {
+        [6702] = {
+            icon = "Interface\\Icons\\inv_belt_02",
+            source = "Sold by Mavralyn (Darkshore), Gina MacGregor (Westfall), Andrew Hilbert (Silverpine Forest)",
+        },
+        [7953] = {
+            icon = "Interface\\Icons\\inv_misc_monsterscales_03",
+            source = "Sold by Kalldan Felmoon (Barrens, near Wailing Caverns)",
+        },
+    },
+    [95] = {
+        [6703] = {
+            icon = "Interface\\Icons\\inv_chest_chain_12",
+            source = "Sold by Mavralyn (Darkshore), Gina MacGregor (Westfall), Andrew Hilbert (Silverpine Forest)",
+        },
+    },
+    [100] = {
+        [24940] = {
+            icon = "Interface\\Icons\\inv_chest_cloth_45",
+            source = "Sold by Amy Davenport (Redridge Mountains, Alliance-only vendor)",
+        },
+        [9070] = {
+            icon = "Interface\\Icons\\inv_misc_monsterscales_03",
+            source = "Sold by Clyde Ranthal (Redridge Mountains, Alliance-only vendor)",
+        },
+    },
+    [105] = {
+        [7954] = {
+            icon = "Interface\\Icons\\inv_gauntlets_05",
+            source = "Sold by Kalldan Felmoon (Barrens, near Wailing Caverns)",
+        },
+    },
     [115] = {
         [7955] = {
             icon = "Interface\\Icons\\inv_belt_09",
             source = "Quest reward: \"Deviate Eradication\" (Wailing Caverns chain)",
+        },
+    },
+    [120] = {
+        [9072] = {
+            icon = "Interface\\Icons\\inv_gauntlets_05",
+            source = "Sold by Wenna Silkbeard (Wetlands, Alliance-only vendor)",
         },
     },
     [135] = {
@@ -11,6 +49,58 @@ TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking = TrainerSpellsBuiltin_Prof
             icon = "Interface\\Icons\\inv_gauntlets_18",
             cost = 1800,
             source = "Sold by Harlown Darkweave (Ashenvale)",
+        },
+    },
+    [150] = {
+        [23190] = {
+            icon = "Interface\\Icons\\inv_misc_throwingball_01",
+            source = "Sold by Bombus Finespindle (Ironforge) / Tamar (Orgrimmar)",
+        },
+    },
+    [155] = {
+        [23399] = {
+            icon = "Interface\\Icons\\inv_bracer_15",
+            source = "Sold by Saenorion (Darnassus) / Joseph Moore (Undercity)",
+        },
+        [3772] = {
+            icon = "Interface\\Icons\\inv_chest_leather_08",
+            source = "Sold by Wenna Silkbeard (Wetlands), George Candarte (Hillsbrad, Horde)",
+        },
+    },
+    [165] = {
+        [4097] = {
+            icon = "Interface\\Icons\\inv_belt_02",
+            source = "Sold by Androd Fadran (Arathi Highlands, Alliance-only vendor)",
+        },
+        [4096] = {
+            icon = "Interface\\Icons\\inv_chest_leather_04",
+            source = "Sold by Tunkk (Arathi Highlands, Hammerfall, Horde-only vendor)",
+        },
+    },
+    [170] = {
+        [7149] = {
+            icon = "Interface\\Icons\\inv_pants_12",
+            source = "Sold by Lardan (Ashenvale), Hammon Karwn (Arathi Highlands), Jandia (Thousand Needles, Horde)",
+        },
+        [6704] = {
+            icon = "Interface\\Icons\\inv_chest_chain_12",
+            source = "Sold by Micha Yance (Hillsbrad), Blixrez Goodstitch (Stranglethorn Vale), Christoph Jeffcoat (Hillsbrad, Horde)",
+        },
+    },
+    [185] = {
+        [3778] = {
+            icon = "Interface\\Icons\\inv_belt_01",
+            source = "Sold by Rikqiz (Stranglethorn Vale, Booty Bay)",
+        },
+    },
+    [190] = {
+        [9202] = {
+            icon = "Interface\\Icons\\inv_bracer_06",
+            source = "Sold by Saenorion (Darnassus), Joseph Moore (Undercity), Pratt McGrubben / Jangdor Swiftstrider (Feralas)",
+        },
+        [6705] = {
+            icon = "Interface\\Icons\\inv_bracer_05",
+            source = "Sold by Blixrez Goodstitch (Stranglethorn Vale), Helenia Olden (Dustwallow Marsh)",
         },
     },
     [200] = {
@@ -27,10 +117,22 @@ TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking = TrainerSpellsBuiltin_Prof
             source = "Sold by Pratt McGrubben / Jangdor Swiftstrider (Feralas); also world drop",
         },
     },
+    [210] = {
+        [10516] = {
+            icon = "Interface\\Icons\\inv_shoulder_07",
+            source = "Sold by Nioma (Hinterlands), Jangdor Swiftstrider (Feralas, Horde)",
+        },
+    },
     [220] = {
         [10525] = {
             icon = "Interface\\Icons\\inv_chest_leather_02",
             source = "Dropped by Wastewander Bandit (Tanaris)",
+        },
+    },
+    [255] = {
+        [19048] = {
+            icon = "Interface\\Icons\\inv_bracer_09",
+            source = "Sold by Zannok Hidepiercer (Silithus)",
         },
     },
     [260] = {
@@ -38,17 +140,35 @@ TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking = TrainerSpellsBuiltin_Prof
             icon = "Interface\\Icons\\inv_gauntlets_31",
             source = "Sold by Leonard Porter (Western Plaguelands) / Werg Thickblade (Tirisfal Glades)",
         },
+        [19050] = {
+            icon = "Interface\\Icons\\inv_chest_chain_06",
+            source = "Sold by Masat T'andr (Swamp of Sorrows); requires Dragonscale specialization",
+            requires = {10656},
+        },
     },
     [265] = {
         [19052] = {
             icon = "Interface\\Icons\\inv_bracer_07",
             source = "Dropped by Legashi Rogue (Azshara)",
         },
+        [19053] = {
+            icon = "Interface\\Icons\\inv_gauntlets_23",
+            source = "Sold by Blimo Gadgetspring (Azshara); requires Tribal specialization",
+            requires = {10660},
+        },
+    },
+    [270] = {
+        [19062] = {
+            icon = "Interface\\Icons\\inv_shoulder_06",
+            source = "Sold by Gigget Zipcoil (Hinterlands); requires Tribal specialization",
+            requires = {10660},
+        },
     },
     [275] = {
         [19068] = {
             icon = "Interface\\Icons\\inv_chest_leather_04",
-            source = "Sold by Meilosh (Felwood, Timbermaw Hold rep); also drops in Dire Maul Tribute run",
+            source = "Sold by Meilosh (Felwood, requires Timbermaw Hold - Friendly); also drops in Dire Maul Tribute run",
+            requires = {10660},
         },
     },
     [280] = {
@@ -63,6 +183,11 @@ TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking = TrainerSpellsBuiltin_Prof
             cost = 5000,
             source = "Sold by Blimo Gadgetspring (Azshara)",
         },
+        [19080] = {
+            icon = "Interface\\Icons\\inv_pants_wolf",
+            source = "Sold by Meilosh (Felwood, requires Timbermaw Hold - Friendly); requires Tribal specialization",
+            requires = {10660},
+        },
     },
     [290] = {
         [19082] = {
@@ -74,6 +199,19 @@ TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking = TrainerSpellsBuiltin_Prof
             icon = "Interface\\Icons\\inv_gauntlets_26",
             source = "Sold by Nergal (Un'Goro Crater)",
         },
+        [19085] = {
+            icon = "Interface\\Icons\\inv_chest_plate06",
+            source = "Sold by Plugger Spazzring (Blackrock Depths, Grim Guzzler); requires Dragonscale specialization",
+            requires = {10656},
+        },
+        [23705] = {
+            icon = "Interface\\Icons\\inv_boots_cloth_08",
+            source = "Sold by Argent Dawn Quartermasters (Tirisfal Glades / Western Plaguelands / Eastern Plaguelands, requires Argent Dawn - Honored)",
+        },
+        [23703] = {
+            icon = "Interface\\Icons\\inv_belt_09",
+            source = "Sold by Meilosh (Felwood, requires Timbermaw Hold - Honored)",
+        },
     },
     [295] = {
         [19088] = {
@@ -84,6 +222,11 @@ TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking = TrainerSpellsBuiltin_Prof
         [19090] = {
             icon = "Interface\\Icons\\inv_shoulder_05",
             source = "Dropped by Son of Arkkoroc (rare, Azshara)",
+        },
+        [20853] = {
+            icon = "Interface\\Icons\\inv_boots_07",
+            source = "Sold by Lokhtos Darkbargainer (Blackrock Depths, requires Thorium Brotherhood - Friendly); requires Tribal specialization",
+            requires = {10660},
         },
     },
     [300] = {
@@ -106,12 +249,14 @@ TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking = TrainerSpellsBuiltin_Prof
         [20854] = {
             icon = "Interface\\Icons\\inv_helmet_08",
             cost = 160000,
-            source = "Sold by Lokhtos Darkbargainer (Blackrock Depths, Thorium Brotherhood rep)",
+            source = "Sold by Lokhtos Darkbargainer (Blackrock Depths, requires Thorium Brotherhood - Friendly); requires Elemental specialization",
+            requires = {10658},
         },
         [20855] = {
             icon = "Interface\\Icons\\inv_boots_plate_09",
             cost = 160000,
-            source = "Sold by Lokhtos Darkbargainer (Blackrock Depths, Thorium Brotherhood rep)",
+            source = "Sold by Lokhtos Darkbargainer (Blackrock Depths, requires Thorium Brotherhood - Honored); requires Dragonscale specialization",
+            requires = {10656},
         },
         [22927] = {
             icon = "Interface\\Icons\\inv_misc_cape_01",
@@ -120,34 +265,98 @@ TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking = TrainerSpellsBuiltin_Prof
         [23708] = {
             icon = "Interface\\Icons\\inv_gauntlets_22",
             cost = 90000,
-            source = "Sold by Lokhtos Darkbargainer (Blackrock Depths, Thorium Brotherhood rep)",
+            source = "Sold by Lokhtos Darkbargainer (Blackrock Depths, requires Thorium Brotherhood - Revered); requires Dragonscale specialization",
+            requires = {10656},
         },
         [23709] = {
             icon = "Interface\\Icons\\inv_belt_24",
-            source = "Sold by Lokhtos Darkbargainer (Blackrock Depths, Thorium Brotherhood rep)",
+            source = "Sold by Lokhtos Darkbargainer (Blackrock Depths, requires Thorium Brotherhood - Revered); requires Tribal specialization",
+            requires = {10660},
         },
         [24121] = {
             icon = "Interface\\Icons\\inv_chest_leather_03",
             cost = 50000,
-            source = "Sold by Rin'wosho the Trader (Stranglethorn Vale, Zandalar Tribe rep)",
+            source = "Sold by Rin'wosho the Trader (Stranglethorn Vale, requires Zandalar Tribe - Revered)",
         },
         [24124] = {
             icon = "Interface\\Icons\\inv_chest_leather_07",
-            source = "Sold by Rin'wosho the Trader (Stranglethorn Vale, Zandalar Tribe rep)",
+            source = "Sold by Rin'wosho the Trader (Stranglethorn Vale, requires Zandalar Tribe - Revered)",
         },
         [24703] = {
             icon = "Interface\\Icons\\inv_chest_plate08",
             cost = 60000,
-            source = "Sold by Aendel Windspear (Silithus, AQ war effort)",
+            source = "Sold by Aendel Windspear (Silithus, requires Cenarion Circle - Exalted); requires Dragonscale specialization",
+            requires = {10656},
         },
         [24848] = {
             icon = "Interface\\Icons\\inv_chest_leather_02",
             cost = 40000,
-            source = "Sold by Aendel Windspear (Silithus, AQ war effort)",
+            source = "Sold by Aendel Windspear (Silithus, requires Cenarion Circle - Revered)",
         },
         [26279] = {
             icon = "Interface\\Icons\\inv_gauntlets_05",
             source = "Dropped by The Windreaver (Silithus) / Princess Tempestria (Winterspring)",
+        },
+        [23706] = {
+            icon = "Interface\\Icons\\inv_shoulder_26",
+            source = "Sold by Argent Dawn Quartermasters (Tirisfal Glades / Western Plaguelands / Eastern Plaguelands, requires Argent Dawn - Revered)",
+        },
+        [28474] = {
+            icon = "Interface\\Icons\\inv_belt_17",
+            source = "Sold by Aendel Windspear (Silithus, requires Cenarion Circle - Friendly)",
+        },
+        [24849] = {
+            icon = "Interface\\Icons\\inv_bracer_12",
+            source = "Sold by Aendel Windspear (Silithus, requires Cenarion Circle - Friendly)",
+        },
+        [24846] = {
+            icon = "Interface\\Icons\\inv_bracer_05",
+            source = "Sold by Aendel Windspear (Silithus, requires Cenarion Circle - Friendly)",
+        },
+        [28473] = {
+            icon = "Interface\\Icons\\inv_boots_cloth_04",
+            source = "Sold by Aendel Windspear (Silithus, requires Cenarion Circle - Honored)",
+        },
+        [24850] = {
+            icon = "Interface\\Icons\\inv_gauntlets_11",
+            source = "Sold by Aendel Windspear (Silithus, requires Cenarion Circle - Honored)",
+        },
+        [24847] = {
+            icon = "Interface\\Icons\\inv_gauntlets_11",
+            source = "Sold by Aendel Windspear (Silithus, requires Cenarion Circle - Honored)",
+        },
+        [28472] = {
+            icon = "Interface\\Icons\\inv_helmet_58",
+            source = "Sold by Aendel Windspear (Silithus, requires Cenarion Circle - Revered)",
+        },
+        [24851] = {
+            icon = "Interface\\Icons\\inv_chest_plate07",
+            source = "Sold by Aendel Windspear (Silithus, requires Cenarion Circle - Revered)",
+        },
+        [23707] = {
+            icon = "Interface\\Icons\\inv_belt_32",
+            source = "Sold by Lokhtos Darkbargainer (Blackrock Depths, requires Thorium Brotherhood - Honored)",
+        },
+        [23710] = {
+            icon = "Interface\\Icons\\inv_belt_13",
+            source = "Sold by Lokhtos Darkbargainer (Blackrock Depths, requires Thorium Brotherhood - Revered); requires Elemental specialization",
+            requires = {10658},
+        },
+        [23704] = {
+            icon = "Interface\\Icons\\inv_gauntlets_26",
+            source = "Sold by Meilosh (Felwood, requires Timbermaw Hold - Revered)",
+        },
+        [24123] = {
+            icon = "Interface\\Icons\\inv_bracer_07",
+            source = "Sold by Rin'wosho the Trader (Stranglethorn Vale, requires Zandalar Tribe - Friendly)",
+        },
+        [24122] = {
+            icon = "Interface\\Icons\\inv_gauntlets_31",
+            source = "Sold by Rin'wosho the Trader (Stranglethorn Vale, requires Zandalar Tribe - Honored)",
+        },
+        [24125] = {
+            icon = "Interface\\Icons\\inv_shoulder_23",
+            source = "Sold by Rin'wosho the Trader (Stranglethorn Vale, requires Zandalar Tribe - Honored)",
         },
     },
 }
