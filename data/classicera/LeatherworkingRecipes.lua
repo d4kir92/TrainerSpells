e6b5f07bb0c5d02e3dcda4ee0ca1aa4428df40c8
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-07-22 14:15:34 +0200

    refactor

--- data/classicera/LeatherworkingRecipes.lua
+++ data/classicera/LeatherworkingRecipes.lua
@@ -7,16 +7,19 @@ TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking = TrainerSpellsBuiltin_Prof
     },
     [135] = {
         [9146] = {
-            source = "Sold by Harlown Darkweave (Moonglade)",
+            cost = 1800,
+            source = "Sold by Harlown Darkweave (Ashenvale)",
         },
     },
     [200] = {
         [22711] = {
+            cost = 3500,
             source = "Sold by Rikqiz (Stranglethorn Vale)",
         },
     },
     [205] = {
         [10509] = {
+            cost = 3500,
             source = "Sold by Pratt McGrubben / Jangdor Swiftstrider (Feralas); also world drop",
         },
     },
@@ -47,11 +50,13 @@ TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking = TrainerSpellsBuiltin_Prof
     },
     [285] = {
         [19077] = {
+            cost = 5000,
             source = "Sold by Blimo Gadgetspring (Azshara)",
         },
     },
     [290] = {
         [19082] = {
+            cost = 22000,
             source = "Sold by Jase Farlane (Eastern Plaguelands)",
         },
         [19084] = {
@@ -60,6 +65,7 @@ TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking = TrainerSpellsBuiltin_Prof
     },
     [295] = {
         [19088] = {
+            cost = 25000,
             source = "Sold by Zannok Hidepiercer (Silithus)",
         },
         [19090] = {
@@ -80,30 +86,36 @@ TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking = TrainerSpellsBuiltin_Prof
             source = "Dropped by Anvilrage Captain (Blackrock Depths)",
         },
         [20854] = {
+            cost = 160000,
             source = "Sold by Lokhtos Darkbargainer (Blackrock Depths, Thorium Brotherhood rep)",
         },
         [20855] = {
+            cost = 160000,
             source = "Sold by Lokhtos Darkbargainer (Blackrock Depths, Thorium Brotherhood rep)",
         },
         [22927] = {
             source = "Found in Knot Thimblejack's Cache (Dire Maul chest)",
         },
         [23708] = {
+            cost = 90000,
             source = "Sold by Lokhtos Darkbargainer (Blackrock Depths, Thorium Brotherhood rep)",
         },
         [23709] = {
             source = "Sold by Lokhtos Darkbargainer (Blackrock Depths, Thorium Brotherhood rep)",
         },
         [24121] = {
+            cost = 50000,
             source = "Sold by Rin'wosho the Trader (Stranglethorn Vale, Zandalar Tribe rep)",
         },
         [24124] = {
             source = "Sold by Rin'wosho the Trader (Stranglethorn Vale, Zandalar Tribe rep)",
         },
         [24703] = {
+            cost = 60000,
             source = "Sold by Aendel Windspear (Silithus, AQ war effort)",
         },
         [24848] = {
+            cost = 40000,
             source = "Sold by Aendel Windspear (Silithus, AQ war effort)",
         },
         [26279] = {
