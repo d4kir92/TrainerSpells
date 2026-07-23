57d14e0863a349ce4ecc62a5a142ab90c1946778
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-07-23 16:00:29 +0200

    enchanting refactor

--- data/classicera/EnchantingRecipes.lua
+++ data/classicera/EnchantingRecipes.lua
@@ -1,9 +1,69 @@
 TrainerSpellsBuiltin_ProfessionRecipe = TrainerSpellsBuiltin_ProfessionRecipe or {}
 TrainerSpellsBuiltin_ProfessionRecipe.Enchanting = TrainerSpellsBuiltin_ProfessionRecipe.Enchanting or {
+    [20] = {
+        [7443] = {
+            icon = "Interface\\Icons\\spell_holy_greaterheal",
+            source = "Sold by Vaean (Darnassus) / Tilli Thistlefuzz (Ironforge) / Jessara Cordell (Stormwind) / Kania (Silithus) / Kithas (Orgrimmar) / Nata Dawnstrider (Thunder Bluff) / Thaddeus Webb (Undercity) / Lilly & Leo Sarn (Silverpine Forest)",
+        },
+    },
+    [45] = {
+        [25124] = {
+            icon = "Interface\\Icons\\inv_poison_mindnumbing",
+            source = "Sold by Vaean (Darnassus) / Tilli Thistlefuzz (Ironforge) / Jessara Cordell (Stormwind) / Kania (Silithus) / Kithas (Orgrimmar) / Nata Dawnstrider (Thunder Bluff) / Thaddeus Webb (Undercity) / Lilly & Leo Sarn (Silverpine Forest)",
+        },
+    },
+    [80] = {
+        [7776] = {
+            icon = "Interface\\Icons\\spell_holy_greaterheal",
+            source = "Horde only. Sold by Kithas (Orgrimmar) / Lilly (Silverpine Forest)",
+        },
+    },
+    [100] = {
+        [7793] = {
+            icon = "Interface\\Icons\\spell_holy_greaterheal",
+            source = "Sold by Tilli Thistlefuzz (Ironforge) / Kithas (Orgrimmar) / Nata Dawnstrider (Thunder Bluff) / Leo Sarn (Silverpine Forest)",
+        },
+    },
+    [110] = {
+        [13419] = {
+            icon = "Interface\\Icons\\spell_holy_greaterheal",
+            source = "Sold by Dalria (Ashenvale, Astranaar) / Kulwia (Stonetalon Mountains, Sun Rock Retreat)",
+        },
+    },
+    [125] = {
+        [7867] = {
+            icon = "Interface\\Icons\\spell_holy_greaterheal",
+            source = "Sold by Zixil (Hillsbrad Foothills, wanders between Southshore and Tarren Mill, sells to both factions) / Nata Dawnstrider (Thunder Bluff)",
+        },
+    },
+    [140] = {
+        [13536] = {
+            icon = "Interface\\Icons\\spell_holy_greaterheal",
+            source = "Sold by Dalria (Ashenvale) / Kulwia (Stonetalon Mountains)",
+        },
+    },
+    [150] = {
+        [25125] = {
+            icon = "Interface\\Icons\\inv_potion_98",
+            source = "Sold by Vaean (Darnassus) / Tilli Thistlefuzz (Ironforge) / Jessara Cordell (Stormwind) / Kania (Silithus) / Kithas (Orgrimmar) / Nata Dawnstrider (Thunder Bluff) / Thaddeus Webb (Undercity) / Lilly (Silverpine Forest)",
+        },
+    },
+    [170] = {
+        [13646] = {
+            icon = "Interface\\Icons\\spell_holy_greaterheal",
+            source = "Sold by Micha Yance (Hillsbrad Foothills, Southshore) / Keena (Arathi Highlands, Hammerfall)",
+        },
+    },
     [175] = {
         [13653] = {
             icon = "Interface\\Icons\\spell_holy_greaterheal",
             source = "Random world drop (BoE, many mobs)",
+        },
+    },
+    [200] = {
+        [25126] = {
+            icon = "Interface\\Icons\\inv_potion_103",
+            source = "Sold by Vaean (Darnassus) / Tilli Thistlefuzz (Ironforge) / Jessara Cordell (Stormwind) / Kania (Silithus) / Kithas (Orgrimmar) / Nata Dawnstrider (Thunder Bluff) / Thaddeus Webb (Undercity) / Lilly (Silverpine Forest)",
         },
     },
     [215] = {
@@ -24,10 +84,20 @@ TrainerSpellsBuiltin_ProfessionRecipe.Enchanting = TrainerSpellsBuiltin_Professi
             source = "Random world drop (BoE, very common, many humanoid mobs)",
         },
     },
+    [235] = {
+        [13931] = {
+            icon = "Interface\\Icons\\spell_holy_greaterheal",
+            source = "Sold by Mythrin'dir (Darnassus) / Banalash (Swamp of Sorrows, Stonard)",
+        },
+    },
     [250] = {
         [13947] = {
             icon = "Interface\\Icons\\spell_holy_greaterheal",
             source = "Random world drop (BoE, many mobs)",
+        },
+        [25127] = {
+            icon = "Interface\\Icons\\inv_potion_99",
+            source = "Sold by Kania (Silithus, Cenarion Circle)",
         },
     },
     [265] = {
@@ -38,10 +108,6 @@ TrainerSpellsBuiltin_ProfessionRecipe.Enchanting = TrainerSpellsBuiltin_Professi
         [15596] = {
             icon = "Interface\\Icons\\inv_misc_gem_bloodstone_01",
             source = "Dropped by Lord Roccor (Blackrock Depths)",
-        },
-        [20015] = {
-            icon = "Interface\\Icons\\spell_holy_greaterheal",
-            source = "Sold by Lorelae Wintersong (Moonglade, Timbermaw Hold rep)",
         },
         [20017] = {
             icon = "Interface\\Icons\\spell_holy_greaterheal",
@@ -58,6 +124,16 @@ TrainerSpellsBuiltin_ProfessionRecipe.Enchanting = TrainerSpellsBuiltin_Professi
             source = "Dropped by Legashi Rogue (Azshara)",
         },
     },
+    [275] = {
+        [20026] = {
+            icon = "Interface\\Icons\\spell_holy_greaterheal",
+            source = "Sold by Qia (Winterspring, Everlook)",
+        },
+        [25128] = {
+            icon = "Interface\\Icons\\inv_potion_104",
+            source = "Sold by Kania (Silithus, Cenarion Circle)",
+        },
+    },
     [280] = {
         [20016] = {
             icon = "Interface\\Icons\\spell_holy_greaterheal",
@@ -68,6 +144,10 @@ TrainerSpellsBuiltin_ProfessionRecipe.Enchanting = TrainerSpellsBuiltin_Professi
         [20010] = {
             icon = "Interface\\Icons\\spell_holy_greaterheal",
             source = "Dropped by Deadwind Warlock (Deadwind Pass)",
+        },
+        [20015] = {
+            icon = "Interface\\Icons\\spell_holy_greaterheal",
+            source = "Sold by Lorelae Wintersong (Moonglade)",
         },
         [20029] = {
             icon = "Interface\\Icons\\spell_holy_greaterheal",
@@ -81,11 +161,23 @@ TrainerSpellsBuiltin_ProfessionRecipe.Enchanting = TrainerSpellsBuiltin_Professi
         },
         [20051] = {
             icon = "Interface\\Icons\\inv_wand_09",
-            source = "Sold by Lorelae Wintersong (Moonglade, Timbermaw Hold rep)",
+            source = "Sold by Lorelae Wintersong (Moonglade)",
         },
         [23799] = {
             icon = "Interface\\Icons\\spell_holy_greaterheal",
-            source = "Sold by Lokhtos Darkbargainer (Blackrock Depths, Thorium Brotherhood rep)",
+            source = "Sold by Lokhtos Darkbargainer (Blackrock Depths, requires Thorium Brotherhood - Friendly)",
+        },
+        [23800] = {
+            icon = "Interface\\Icons\\spell_holy_greaterheal",
+            source = "Sold by Meilosh (Felwood, Timbermaw Hold, requires Timbermaw Hold - Honored)",
+        },
+        [23801] = {
+            icon = "Interface\\Icons\\spell_holy_greaterheal",
+            source = "Sold by Argent Quartermaster Hasana (Tirisfal Glades) / Lightspark (Western Plaguelands) / Miranda Breechlock (Eastern Plaguelands) (requires Argent Dawn - Honored)",
+        },
+        [27837] = {
+            icon = "Interface\\Icons\\spell_holy_greaterheal",
+            source = "Sold by Meilosh (Felwood, Timbermaw Hold, requires Timbermaw Hold - Friendly)",
         },
     },
     [295] = {
@@ -127,17 +219,33 @@ TrainerSpellsBuiltin_ProfessionRecipe.Enchanting = TrainerSpellsBuiltin_Professi
             icon = "Interface\\Icons\\spell_holy_greaterheal",
             source = "Drops from Molten Core bosses and the Emerald Dragons (BoE)",
         },
+        [23802] = {
+            icon = "Interface\\Icons\\spell_holy_greaterheal",
+            source = "Sold by Argent Quartermaster Hasana (Tirisfal Glades) / Lightspark (Western Plaguelands) / Miranda Breechlock (Eastern Plaguelands) (requires Argent Dawn - Revered)",
+        },
         [23803] = {
             icon = "Interface\\Icons\\spell_holy_greaterheal",
-            source = "Sold by Lokhtos Darkbargainer (Blackrock Depths, Thorium Brotherhood rep)",
+            source = "Sold by Lokhtos Darkbargainer (Blackrock Depths, requires Thorium Brotherhood - Honored)",
         },
         [23804] = {
             icon = "Interface\\Icons\\spell_holy_greaterheal",
-            source = "Sold by Lokhtos Darkbargainer (Blackrock Depths, Thorium Brotherhood rep)",
+            source = "Sold by Lokhtos Darkbargainer (Blackrock Depths, requires Thorium Brotherhood - Revered)",
+        },
+        [25081] = {
+            icon = "Interface\\Icons\\spell_holy_greaterheal",
+            source = "Sold by Kania (Silithus, Cenarion Circle, requires Cenarion Circle - Friendly)",
         },
         [25082] = {
             icon = "Interface\\Icons\\spell_holy_greaterheal",
-            source = "Sold by Kania (Silithus, Cenarion Circle/AQ war effort)",
+            source = "Sold by Kania (Silithus, Cenarion Circle, requires Cenarion Circle - Honored)",
+        },
+        [25137] = {
+            icon = "Interface\\Icons\\inv_potion_105",
+            source = "Sold by Rin'wosho the Trader (Stranglethorn Vale, requires Zandalar Tribe - Honored)",
+        },
+        [25138] = {
+            icon = "Interface\\Icons\\inv_potion_100",
+            source = "Sold by Rin'wosho the Trader (Stranglethorn Vale, requires Zandalar Tribe - Friendly)",
         },
     },
 }
