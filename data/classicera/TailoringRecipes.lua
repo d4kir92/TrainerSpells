98650719eb995d27bb75c2f2161a8ae1804b3f96
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-07-23 21:42:02 +0200

    Tailorring refactor

--- data/classicera/TailoringRecipes.lua
+++ data/classicera/TailoringRecipes.lua
@@ -1,5 +1,37 @@
 TrainerSpellsBuiltin_ProfessionRecipe = TrainerSpellsBuiltin_ProfessionRecipe or {}
 TrainerSpellsBuiltin_ProfessionRecipe.Tailoring = TrainerSpellsBuiltin_ProfessionRecipe.Tailoring or {
+    [55] = {
+        [7630] = {
+            icon = "Interface\\Icons\\inv_chest_cloth_37",
+            source = "Sold by Tharynn Bouden (Elwynn Forest, Goldshire), Valdaron (Darkshore, Auberdine), Borya (Orgrimmar), Constance Brisboise (Tirisfal Glades), Wrahk (Barrens, Crossroads)",
+        },
+    },
+    [70] = {
+        [6686] = {
+            icon = "Interface\\Icons\\inv_misc_bag_02",
+            source = "Sold by Valdaron (Darkshore, Auberdine), Gina MacGregor (Westfall), Mahu (Thunder Bluff), Andrew Hilbert (Silverpine Forest)",
+        },
+        [7633] = {
+            icon = "Interface\\Icons\\inv_chest_cloth_23",
+            source = "Sold by Elynna (Darnassus), Drake Lindgren (Elwynn Forest), Andrew Hilbert (Silverpine Forest)",
+        },
+    },
+    [100] = {
+        [7639] = {
+            icon = "Interface\\Icons\\inv_shirt_13",
+            source = "Sold by Alexandra Bolero (Stormwind), Gina MacGregor (Westfall), Borya (Orgrimmar), Yonada (Barrens, Camp Taurajo), Mallen Swain (Hillsbrad Foothills, Tarren Mill)",
+        },
+    },
+    [115] = {
+        [6688] = {
+            icon = "Interface\\Icons\\inv_misc_bag_10_red",
+            source = "Sold by Valdaron (Darkshore), Rann Flamespinner (Loch Modan), Kiknikle (Barrens), Amy Davenport (Redridge Mountains), Jennabink Powerseam (Wetlands), Borya (Orgrimmar), Mahu (Thunder Bluff), Millie Gregorian (Undercity), Zixil (Hillsbrad Foothills), Wrahk (Barrens), Yonada (Barrens)",
+        },
+        [7643] = {
+            icon = "Interface\\Icons\\inv_chest_cloth_24",
+            source = "Sold by Elynna (Darnassus), Rann Flamespinner (Loch Modan), Ranik (Barrens, Ratchet), Sheri Zipstitch (Duskwood), Jennabink Powerseam (Wetlands)",
+        },
+    },
     [245] = {
         [12086] = {
             icon = "Interface\\Icons\\inv_helmet_27",
@@ -9,19 +41,31 @@ TrainerSpellsBuiltin_ProfessionRecipe.Tailoring = TrainerSpellsBuiltin_Professio
     [250] = {
         [18560] = {
             icon = "Interface\\Icons\\inv_fabric_moonrag_01",
-            source = "Sold by Qia (Winterspring, Timbermaw Hold rep)",
+            source = "Sold by Qia (Winterspring, Everlook)",
         },
     },
     [260] = {
         [18405] = {
             icon = "Interface\\Icons\\inv_misc_bag_19",
-            source = "Sold by Qia (Winterspring, Timbermaw Hold rep)",
+            source = "Sold by Qia (Winterspring, Everlook)",
+        },
+        [18475] = {
+            icon = "Interface\\Icons\\inv_chest_cloth_04",
+            source = "Sold by Darnall (Moonglade, Nighthaven)",
+        },
+        [26088] = {
+            icon = "Interface\\Icons\\inv_misc_bag_soulbag",
+            source = "Sold by Vizzklick (Tanaris, Gadgetzan)",
         },
     },
     [265] = {
         [18410] = {
             icon = "Interface\\Icons\\inv_belt_16",
             source = "Dropped by Lingering Highborne (Azshara)",
+        },
+        [18479] = {
+            icon = "Interface\\Icons\\inv_misc_cape_10",
+            source = "Sold by Darnall (Moonglade, Nighthaven)",
         },
     },
     [275] = {
@@ -35,21 +79,45 @@ TrainerSpellsBuiltin_ProfessionRecipe.Tailoring = TrainerSpellsBuiltin_Professio
         },
         [18419] = {
             icon = "Interface\\Icons\\inv_pants_06",
-            source = "Sold by Lorelae Wintersong (Moonglade, Timbermaw Hold rep)",
+            source = "Sold by Lorelae Wintersong (Moonglade)",
         },
         [18422] = {
             icon = "Interface\\Icons\\inv_misc_cape_18",
             source = "Dropped by Overmaster Pyron (rare mob)",
         },
+        [18491] = {
+            icon = "Interface\\Icons\\inv_gauntlets_25",
+            source = "Sold by Qia (Winterspring, Everlook)",
+        },
         [27724] = {
             icon = "Interface\\Icons\\inv_misc_bag_cenarionherbbag",
-            source = "Sold by Mishta (Silithus, Cenarion Circle rep)",
+            source = "Sold by Mishta (Silithus, Cenarion Circle rep, requires Friendly)",
+        },
+        [27727] = {
+            icon = "Interface\\Icons\\inv_misc_bag_enchantedrunecloth",
+            source = "Sold by Kania (Silithus)",
+        },
+    },
+    [280] = {
+        [18497] = {
+            icon = "Interface\\Icons\\inv_boots_05",
+            source = "Sold by Darnall (Moonglade, Nighthaven)",
         },
     },
     [285] = {
         [18436] = {
             icon = "Interface\\Icons\\inv_chest_cloth_49",
             source = "Dropped by Cobalt Mageweaver (Winterspring)",
+        },
+    },
+    [290] = {
+        [23662] = {
+            icon = "Interface\\Icons\\inv_belt_09",
+            source = "Sold by Meilosh, Felwood (requires Timbermaw Hold - Honored)",
+        },
+        [23664] = {
+            icon = "Interface\\Icons\\inv_boots_cloth_03",
+            source = "Sold by Argent Quartermaster Hasana (Tirisfal Glades), Lightspark (Western Plaguelands) & Miranda Breechlock (Eastern Plaguelands) (requires Argent Dawn - Honored)",
         },
     },
     [300] = {
@@ -67,7 +135,11 @@ TrainerSpellsBuiltin_ProfessionRecipe.Tailoring = TrainerSpellsBuiltin_Professio
         },
         [20848] = {
             icon = "Interface\\Icons\\inv_shoulder_23",
-            source = "Sold by Lokhtos Darkbargainer (Blackrock Depths, Thorium Brotherhood rep)",
+            source = "Sold by Lokhtos Darkbargainer (Blackrock Depths, Thorium Brotherhood rep, requires Honored)",
+        },
+        [20849] = {
+            icon = "Interface\\Icons\\inv_gauntlets_26",
+            source = "Sold by Lokhtos Darkbargainer, Blackrock Depths (requires Thorium Brotherhood - Friendly)",
         },
         [22866] = {
             icon = "Interface\\Icons\\inv_belt_31",
@@ -87,19 +159,35 @@ TrainerSpellsBuiltin_ProfessionRecipe.Tailoring = TrainerSpellsBuiltin_Professio
         },
         [22902] = {
             icon = "Interface\\Icons\\inv_chest_cloth_04",
-            source = "Sold by Shen'dralar Provisioner (Dire Maul, Shen'dralar rep)",
+            source = "Sold by Shen'dralar Provisioner (Dire Maul, Library)",
+        },
+        [23663] = {
+            icon = "Interface\\Icons\\inv_shoulder_19",
+            source = "Sold by Meilosh, Felwood (requires Timbermaw Hold - Revered)",
+        },
+        [23665] = {
+            icon = "Interface\\Icons\\inv_shoulder_13",
+            source = "Sold by Argent Quartermaster Hasana (Tirisfal Glades), Lightspark (Western Plaguelands) & Miranda Breechlock (Eastern Plaguelands) (requires Argent Dawn - Revered)",
         },
         [23666] = {
             icon = "Interface\\Icons\\inv_chest_cloth_18",
-            source = "Sold by Lokhtos Darkbargainer (Blackrock Depths, Thorium Brotherhood rep)",
+            source = "Sold by Lokhtos Darkbargainer (Blackrock Depths, Thorium Brotherhood rep, requires Honored)",
         },
         [23667] = {
             icon = "Interface\\Icons\\inv_pants_06",
-            source = "Sold by Lokhtos Darkbargainer (Blackrock Depths, Thorium Brotherhood rep)",
+            source = "Sold by Lokhtos Darkbargainer (Blackrock Depths, Thorium Brotherhood rep, requires Revered)",
         },
         [24091] = {
             icon = "Interface\\Icons\\inv_chest_cloth_07",
-            source = "Sold by Rin'wosho the Trader (Stranglethorn Vale, Zandalar Tribe rep)",
+            source = "Sold by Rin'wosho the Trader (Stranglethorn Vale, Zandalar Tribe rep, requires Revered)",
+        },
+        [24092] = {
+            icon = "Interface\\Icons\\inv_pants_cloth_14",
+            source = "Sold by Rin'wosho the Trader, Stranglethorn Vale (requires Zandalar Tribe - Honored)",
+        },
+        [24093] = {
+            icon = "Interface\\Icons\\inv_boots_cloth_02",
+            source = "Sold by Rin'wosho the Trader, Stranglethorn Vale (requires Zandalar Tribe - Friendly)",
         },
         [27660] = {
             icon = "Interface\\Icons\\inv_misc_bag_bigbagofenchantments",
@@ -107,23 +195,23 @@ TrainerSpellsBuiltin_ProfessionRecipe.Tailoring = TrainerSpellsBuiltin_Professio
         },
         [27725] = {
             icon = "Interface\\Icons\\inv_misc_bag_satchelofcenarius",
-            source = "Sold by Mishta (Silithus, Cenarion Circle rep)",
+            source = "Sold by Mishta (Silithus, Cenarion Circle rep, requires Revered)",
         },
         [28210] = {
             icon = "Interface\\Icons\\inv_misc_cape_17",
-            source = "Sold by Mishta (Silithus, Cenarion Circle rep)",
+            source = "Sold by Mishta (Silithus, Cenarion Circle rep, requires Revered)",
         },
         [28480] = {
             icon = "Interface\\Icons\\inv_chest_plate07",
-            source = "Sold by Mishta (Silithus, Cenarion Circle rep)",
+            source = "Sold by Mishta (Silithus, Cenarion Circle rep, requires Revered)",
         },
         [28481] = {
             icon = "Interface\\Icons\\inv_crown_01",
-            source = "Sold by Mishta (Silithus, Cenarion Circle rep)",
+            source = "Sold by Mishta (Silithus, Cenarion Circle rep, requires Honored)",
         },
         [28482] = {
             icon = "Interface\\Icons\\inv_shoulder_18",
-            source = "Sold by Mishta (Silithus, Cenarion Circle rep)",
+            source = "Sold by Mishta (Silithus, Cenarion Circle rep, requires Friendly)",
         },
     },
 }
