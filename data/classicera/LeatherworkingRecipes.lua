f33720380888d738dd4a75449f5fccd7b54ed573
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-07-26 15:53:13 +0200

    v0.3.2

--- data/classicera/LeatherworkingRecipes.lua
+++ data/classicera/LeatherworkingRecipes.lua
@@ -1,5 +1,29 @@
 TrainerSpellsBuiltin_ProfessionRecipe = TrainerSpellsBuiltin_ProfessionRecipe or {}
 TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking = TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking or {
+    [35] = {
+        [9064] = {
+            icon = "Interface\\Icons\\inv_pants_02",
+            source = "World drop",
+        },
+    },
+    [40] = {
+        [5244] = {
+            icon = "Interface\\Icons\\inv_misc_bag_10",
+            source = "Quest reward: \"Kodo Hide Bag\"",
+        },
+    },
+    [60] = {
+        [2163] = {
+            icon = "Interface\\Icons\\inv_chest_wolf",
+            source = "World drop",
+        },
+    },
+    [75] = {
+        [2164] = {
+            icon = "Interface\\Icons\\inv_gauntlets_05",
+            source = "World drop",
+        },
+    },
     [90] = {
         [6702] = {
             icon = "Interface\\Icons\\inv_belt_02",
@@ -8,6 +32,14 @@ TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking = TrainerSpellsBuiltin_Prof
         [7953] = {
             icon = "Interface\\Icons\\inv_misc_monsterscales_03",
             source = "Sold by Kalldan Felmoon (Barrens, near Wailing Caverns)",
+        },
+        [2158] = {
+            icon = "Interface\\Icons\\inv_boots_06",
+            source = "World drop",
+        },
+        [8322] = {
+            icon = "Interface\\Icons\\inv_chest_leather_09",
+            source = "Quest reward: \"Moonglow Vest\"",
         },
     },
     [95] = {
@@ -25,11 +57,23 @@ TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking = TrainerSpellsBuiltin_Prof
             icon = "Interface\\Icons\\inv_misc_monsterscales_03",
             source = "Sold by Clyde Ranthal (Redridge Mountains, Alliance-only vendor)",
         },
+        [2169] = {
+            icon = "Interface\\Icons\\inv_chest_leather_03",
+            source = "World drop",
+        },
+        [3762] = {
+            icon = "Interface\\Icons\\inv_chest_leather_10",
+            source = "World drop",
+        },
     },
     [105] = {
         [7954] = {
             icon = "Interface\\Icons\\inv_gauntlets_05",
             source = "Sold by Kalldan Felmoon (Barrens, near Wailing Caverns)",
+        },
+        [7133] = {
+            icon = "Interface\\Icons\\inv_pants_02",
+            source = "World drop",
         },
     },
     [115] = {
@@ -43,6 +87,14 @@ TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking = TrainerSpellsBuiltin_Prof
             icon = "Interface\\Icons\\inv_gauntlets_05",
             source = "Sold by Wenna Silkbeard (Wetlands, Alliance-only vendor)",
         },
+        [3765] = {
+            icon = "Interface\\Icons\\inv_gauntlets_05",
+            source = "World drop",
+        },
+        [3767] = {
+            icon = "Interface\\Icons\\inv_belt_06",
+            source = "World drop",
+        },
     },
     [135] = {
         [9146] = {
@@ -50,11 +102,35 @@ TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking = TrainerSpellsBuiltin_Prof
             cost = 1800,
             source = "Sold by Harlown Darkweave (Ashenvale)",
         },
+        [9147] = {
+            icon = "Interface\\Icons\\inv_shoulder_06",
+            source = "Sold by Zixil (Hillsbrad Foothills)",
+        },
+    },
+    [140] = {
+        [3769] = {
+            icon = "Interface\\Icons\\inv_shoulder_12",
+            source = "World drop",
+        },
+        [9148] = {
+            icon = "Interface\\Icons\\inv_gauntlets_05",
+            source = "World drop",
+        },
+    },
+    [145] = {
+        [9149] = {
+            icon = "Interface\\Icons\\inv_gauntlets_23",
+            source = "World drop",
+        },
     },
     [150] = {
         [23190] = {
             icon = "Interface\\Icons\\inv_misc_throwingball_01",
             source = "Sold by Bombus Finespindle (Ironforge) / Tamar (Orgrimmar)",
+        },
+        [3771] = {
+            icon = "Interface\\Icons\\inv_gauntlets_05",
+            source = "World drop",
         },
     },
     [155] = {
@@ -76,6 +152,10 @@ TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking = TrainerSpellsBuiltin_Prof
             icon = "Interface\\Icons\\inv_chest_leather_04",
             source = "Sold by Tunkk (Arathi Highlands, Hammerfall, Horde-only vendor)",
         },
+        [9195] = {
+            icon = "Interface\\Icons\\inv_pants_07",
+            source = "World drop",
+        },
     },
     [170] = {
         [7149] = {
@@ -86,11 +166,29 @@ TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking = TrainerSpellsBuiltin_Prof
             icon = "Interface\\Icons\\inv_chest_chain_12",
             source = "Sold by Micha Yance (Hillsbrad), Blixrez Goodstitch (Stranglethorn Vale), Christoph Jeffcoat (Hillsbrad, Horde)",
         },
+        [3775] = {
+            icon = "Interface\\Icons\\inv_belt_03",
+            source = "World drop, also found in chests and lockboxes",
+        },
+    },
+    [175] = {
+        [3773] = {
+            icon = "Interface\\Icons\\inv_chest_leather_08",
+            source = "World drop",
+        },
+        [9197] = {
+            icon = "Interface\\Icons\\inv_chest_chain_09",
+            source = "World drop",
+        },
     },
     [185] = {
         [3778] = {
             icon = "Interface\\Icons\\inv_belt_01",
             source = "Sold by Rikqiz (Stranglethorn Vale, Booty Bay)",
+        },
+        [7153] = {
+            icon = "Interface\\Icons\\inv_misc_cape_03",
+            source = "World drop",
         },
     },
     [190] = {
@@ -102,12 +200,38 @@ TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking = TrainerSpellsBuiltin_Prof
             icon = "Interface\\Icons\\inv_bracer_05",
             source = "Sold by Blixrez Goodstitch (Stranglethorn Vale), Helenia Olden (Dustwallow Marsh)",
         },
+        [21943] = {
+            icon = "Interface\\Icons\\inv_gauntlets_21",
+            source = "Found in Smokywood Pastures Special Gift and Ticking Present (Feast of Winter Veil)",
+        },
+    },
+    [195] = {
+        [3777] = {
+            icon = "Interface\\Icons\\inv_bracer_10",
+            source = "World drop",
+        },
     },
     [200] = {
         [22711] = {
             icon = "Interface\\Icons\\inv_gauntlets_32",
             cost = 3500,
             source = "Sold by Rikqiz (Stranglethorn Vale)",
+        },
+        [3779] = {
+            icon = "Interface\\Icons\\inv_belt_09",
+            source = "World drop, also found in chests and lockboxes",
+        },
+        [9207] = {
+            icon = "Interface\\Icons\\inv_boots_07",
+            source = "World drop, also found in chests and lockboxes",
+        },
+        [9208] = {
+            icon = "Interface\\Icons\\inv_boots_08",
+            source = "World drop",
+        },
+        [10490] = {
+            icon = "Interface\\Icons\\inv_helmet_15",
+            source = "World drop, also found in chests and lockboxes",
         },
     },
     [205] = {
@@ -123,10 +247,86 @@ TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking = TrainerSpellsBuiltin_Prof
             source = "Sold by Nioma (Hinterlands), Jangdor Swiftstrider (Feralas, Horde)",
         },
     },
+    [215] = {
+        [10520] = {
+            icon = "Interface\\Icons\\inv_chest_cloth_25",
+            source = "World drop",
+        },
+    },
     [220] = {
         [10525] = {
             icon = "Interface\\Icons\\inv_chest_leather_02",
             source = "Dropped by Wastewander Bandit (Tanaris)",
+        },
+        [10529] = {
+            icon = "Interface\\Icons\\inv_shoulder_18",
+            source = "Quest reward: \"Wild Leather Shoulders\"",
+        },
+        [10531] = {
+            icon = "Interface\\Icons\\inv_banner_01",
+            source = "World drop",
+        },
+        [10533] = {
+            icon = "Interface\\Icons\\inv_bracer_09",
+            source = "Dropped by Wastewander Shadow Mage (Tanaris)",
+        },
+    },
+    [225] = {
+        [10542] = {
+            icon = "Interface\\Icons\\inv_gauntlets_24",
+            source = "Dropped by Wastewander Thief (Tanaris)",
+        },
+        [10544] = {
+            icon = "Interface\\Icons\\inv_chest_cloth_06",
+            source = "Quest reward: \"Wild Leather Vest\"",
+        },
+        [10546] = {
+            icon = "Interface\\Icons\\inv_helmet_10",
+            source = "Quest reward: \"Wild Leather Helmet\"",
+        },
+    },
+    [235] = {
+        [10554] = {
+            icon = "Interface\\Icons\\inv_boots_05",
+            source = "Dropped by Wastewander Rogue (Tanaris)",
+        },
+    },
+    [240] = {
+        [10560] = {
+            icon = "Interface\\Icons\\inv_pants_02",
+            source = "World drop",
+        },
+        [10562] = {
+            icon = "Interface\\Icons\\inv_misc_cape_02",
+            source = "World drop",
+        },
+        [10564] = {
+            icon = "Interface\\Icons\\inv_shoulder_04",
+            source = "Dropped by Wastewander Assassin, Wastewander Scofflaw and Andre Firebeard (Tanaris)",
+        },
+    },
+    [245] = {
+        [10566] = {
+            icon = "Interface\\Icons\\inv_boots_07",
+            source = "Quest reward: \"Wild Leather Boots\"",
+        },
+        [10568] = {
+            icon = "Interface\\Icons\\inv_pants_12",
+            source = "Dropped by Wastewander Rogue (Tanaris)",
+        },
+    },
+    [250] = {
+        [10570] = {
+            icon = "Interface\\Icons\\inv_helmet_20",
+            source = "Dropped by Wastewander Assassin, Wastewander Scofflaw and Andre Firebeard (Tanaris)",
+        },
+        [10572] = {
+            icon = "Interface\\Icons\\inv_pants_14",
+            source = "Quest reward: \"Wild Leather Leggings\"",
+        },
+        [10574] = {
+            icon = "Interface\\Icons\\inv_misc_cape_03",
+            source = "Quest reward: \"Master of the Wild Leather\"",
         },
     },
     [255] = {
@@ -156,12 +356,35 @@ TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking = TrainerSpellsBuiltin_Prof
             source = "Sold by Blimo Gadgetspring (Azshara); requires Tribal specialization",
             requires = {10660},
         },
+        [19051] = {
+            icon = "Interface\\Icons\\inv_chest_chain_15",
+            source = "Dropped by Portal Seeker (Blasted Lands)",
+        },
     },
     [270] = {
         [19062] = {
             icon = "Interface\\Icons\\inv_shoulder_06",
             source = "Sold by Gigget Zipcoil (Hinterlands); requires Tribal specialization",
             requires = {10660},
+        },
+        [19055] = {
+            icon = "Interface\\Icons\\inv_gauntlets_31",
+            source = "World drop",
+        },
+        [19059] = {
+            icon = "Interface\\Icons\\inv_pants_06",
+            source = "Dropped by Firegut Brute (Burning Steppes); requires Elemental specialization",
+            requires = {10658},
+        },
+        [19060] = {
+            icon = "Interface\\Icons\\inv_pants_05",
+            source = "Dropped by Murk Worm (The Temple of Atal'Hakkar); requires Dragonscale specialization",
+            requires = {10656},
+        },
+        [19061] = {
+            icon = "Interface\\Icons\\inv_shoulder_18",
+            source = "Sold by Pratt McGrubben / Jangdor Swiftstrider (Feralas); requires Elemental specialization",
+            requires = {10658},
         },
     },
     [275] = {
@@ -170,11 +393,47 @@ TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking = TrainerSpellsBuiltin_Prof
             source = "Sold by Meilosh (Felwood, requires Timbermaw Hold - Friendly); also drops in Dire Maul Tribute run",
             requires = {10660},
         },
+        [19063] = {
+            icon = "Interface\\Icons\\inv_boots_07",
+            source = "World drop; requires Tribal specialization",
+            requires = {10660},
+        },
+        [19064] = {
+            icon = "Interface\\Icons\\inv_gauntlets_24",
+            source = "Dropped by Blackrock Soldier (Burning Steppes)",
+        },
+        [19065] = {
+            icon = "Interface\\Icons\\inv_bracer_11",
+            source = "Dropped by Jaedenar Cultist (Felwood)",
+        },
+        [19066] = {
+            icon = "Interface\\Icons\\inv_boots_05",
+            source = "Sold by Qia (Winterspring, Everlook); requires Tribal specialization",
+            requires = {10660},
+        },
+        [19067] = {
+            icon = "Interface\\Icons\\inv_pants_09",
+            source = "Sold by Leonard Porter (Western Plaguelands) / Werg Thickblade (Tirisfal Glades); requires Elemental specialization",
+            requires = {10658},
+        },
     },
     [280] = {
         [19071] = {
             icon = "Interface\\Icons\\inv_misc_bandage_13",
             source = "Dropped by Jadefire Trickster (Felwood)",
+        },
+        [19070] = {
+            icon = "Interface\\Icons\\inv_belt_03",
+            source = "World drop",
+        },
+        [19072] = {
+            icon = "Interface\\Icons\\inv_belt_23",
+            source = "World drop",
+        },
+        [19073] = {
+            icon = "Interface\\Icons\\inv_pants_02",
+            source = "World drop; requires Tribal specialization",
+            requires = {10660},
         },
     },
     [285] = {
@@ -187,6 +446,30 @@ TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking = TrainerSpellsBuiltin_Prof
             icon = "Interface\\Icons\\inv_pants_wolf",
             source = "Sold by Meilosh (Felwood, requires Timbermaw Hold - Friendly); requires Tribal specialization",
             requires = {10660},
+        },
+        [19074] = {
+            icon = "Interface\\Icons\\inv_pants_01",
+            source = "Dropped by Winterfall Den Watcher (Winterspring); requires Tribal specialization",
+            requires = {10660},
+        },
+        [19075] = {
+            icon = "Interface\\Icons\\inv_pants_12",
+            source = "Dropped by Blackrock Slayer (Burning Steppes)",
+        },
+        [19076] = {
+            icon = "Interface\\Icons\\inv_chest_leather_07",
+            source = "Dropped by Firebrand Grunt (Blackrock Spire); requires Elemental specialization",
+            requires = {10658},
+        },
+        [19078] = {
+            icon = "Interface\\Icons\\inv_pants_05",
+            source = "Dropped by Deadwood Shaman (Felwood); requires Elemental specialization",
+            requires = {10658},
+        },
+        [19079] = {
+            icon = "Interface\\Icons\\inv_chest_leather_08",
+            source = "Dropped by Arkkoran Oracle (Azshara); requires Elemental specialization",
+            requires = {10658},
         },
     },
     [290] = {
@@ -212,6 +495,20 @@ TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking = TrainerSpellsBuiltin_Prof
             icon = "Interface\\Icons\\inv_belt_09",
             source = "Sold by Meilosh (Felwood, requires Timbermaw Hold - Honored)",
         },
+        [19081] = {
+            icon = "Interface\\Icons\\inv_chest_leather_07",
+            source = "World drop; requires Tribal specialization",
+            requires = {10660},
+        },
+        [19083] = {
+            icon = "Interface\\Icons\\inv_pants_12",
+            source = "World drop",
+        },
+        [19086] = {
+            icon = "Interface\\Icons\\inv_chest_leather_06",
+            source = "Dropped by Vilebranch Hideskinner (The Hinterlands); requires Tribal specialization",
+            requires = {10660},
+        },
     },
     [295] = {
         [19088] = {
@@ -227,6 +524,16 @@ TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking = TrainerSpellsBuiltin_Prof
             icon = "Interface\\Icons\\inv_boots_07",
             source = "Sold by Lokhtos Darkbargainer (Blackrock Depths, requires Thorium Brotherhood - Friendly); requires Tribal specialization",
             requires = {10660},
+        },
+        [19087] = {
+            icon = "Interface\\Icons\\inv_gauntlets_16",
+            source = "Dropped by Winterfall Totemic (Winterspring); requires Tribal specialization",
+            requires = {10660},
+        },
+        [19089] = {
+            icon = "Interface\\Icons\\inv_shoulder_18",
+            source = "Dropped by Cliff Breaker (Azshara); requires Dragonscale specialization",
+            requires = {10656},
         },
     },
     [300] = {
@@ -357,6 +664,71 @@ TrainerSpellsBuiltin_ProfessionRecipe.Leatherworking = TrainerSpellsBuiltin_Prof
         [24125] = {
             icon = "Interface\\Icons\\inv_shoulder_23",
             source = "Sold by Rin'wosho the Trader (Stranglethorn Vale, requires Zandalar Tribe - Honored)",
+        },
+        [19091] = {
+            icon = "Interface\\Icons\\inv_pants_02",
+            source = "World drop",
+        },
+        [19092] = {
+            icon = "Interface\\Icons\\inv_belt_03",
+            source = "Dropped by Ghoul Ravener (Stratholme)",
+        },
+        [19094] = {
+            icon = "Interface\\Icons\\inv_shoulder_01",
+            source = "Dropped by Anvilrage Marshal (Blackrock Depths); requires Dragonscale specialization",
+            requires = {10656},
+        },
+        [19095] = {
+            icon = "Interface\\Icons\\inv_chest_plate07",
+            source = "Dropped by Decaying Horror (Western Plaguelands); requires Elemental specialization",
+            requires = {10658},
+        },
+        [19098] = {
+            icon = "Interface\\Icons\\inv_chest_plate06",
+            source = "Dropped by Spectral Researcher (Scholomance)",
+        },
+        [19100] = {
+            icon = "Interface\\Icons\\inv_shoulder_07",
+            source = "Dropped by Blackrock Battlemaster (Burning Steppes)",
+        },
+        [19101] = {
+            icon = "Interface\\Icons\\inv_shoulder_13",
+            source = "Dropped by Firebrand Legionnaire (Blackrock Spire); requires Elemental specialization",
+            requires = {10658},
+        },
+        [19102] = {
+            icon = "Interface\\Icons\\inv_chest_leather_07",
+            source = "Dropped by Scholomance Dark Summoner (Scholomance)",
+        },
+        [19103] = {
+            icon = "Interface\\Icons\\inv_shoulder_15",
+            source = "Dropped by Crimson Battle Mage (Stratholme)",
+        },
+        [22727] = {
+            icon = "Interface\\Icons\\inv_misc_armorkit_05",
+            source = "World drop",
+        },
+        [22921] = {
+            icon = "Interface\\Icons\\inv_belt_26",
+            source = "Found in Knot Thimblejack's Cache (Dire Maul chest)",
+        },
+        [22922] = {
+            icon = "Interface\\Icons\\inv_boots_08",
+            source = "Found in Knot Thimblejack's Cache (Dire Maul chest)",
+        },
+        [22923] = {
+            icon = "Interface\\Icons\\inv_bracer_05",
+            source = "Found in Knot Thimblejack's Cache (Dire Maul chest)",
+        },
+        [22926] = {
+            icon = "Interface\\Icons\\inv_misc_cape_02",
+            source = "Found in Knot Thimblejack's Cache (Dire Maul chest); requires Dragonscale specialization",
+            requires = {10656},
+        },
+        [22928] = {
+            icon = "Interface\\Icons\\inv_misc_cape_20",
+            source = "Found in Knot Thimblejack's Cache (Dire Maul chest); requires Elemental specialization",
+            requires = {10658},
         },
     },
 }
