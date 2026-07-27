76e741a9c43b4d35f181b4a100febde21866e012
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-07-27 12:24:39 +0200

    refactor

---
+++ data/tbc/CookingRecipes.lua
@@ -0,0 +1,150 @@
+TrainerSpellsBuiltin_AddProfessionRecipes("Cooking", {
+    [1] = {
+        [33276] = {
+            icon = "Interface\\Icons\\inv_misc_food_47",
+            source = "Sold by Landraelanis (Eversong Woods, Horde)",
+        },
+        [33277] = {
+            icon = "Interface\\Icons\\inv_misc_food_60",
+            source = "Quest reward: \"The Great Moongraze Hunt\"",
+        },
+        [43779] = {
+            icon = "Interface\\Icons\\inv_misc_celebrationcake_01",
+            source = "Found in Crate of Meat, Barrel of Fish",
+        },
+    },
+    [50] = {
+        [33278] = {
+            icon = "Interface\\Icons\\inv_misc_food_86_basilisk",
+            source = "Sold by Master Chef Mouldier (Ghostlands, Horde)",
+        },
+    },
+    [60] = {
+        [28267] = {
+            icon = "Interface\\Icons\\inv_misc_food_88_ravagernuggets",
+            source = "Sold by Master Chef Mouldier (Ghostlands, Horde) / Fazu (Bloodmyst Isle, Alliance)",
+        },
+    },
+    [100] = {
+        [45695] = {
+            icon = "Interface\\Icons\\inv_drink_03",
+            source = "Found in Bag of Fishing Treasures",
+        },
+    },
+    [250] = {
+        [46684] = {
+            icon = "Interface\\Icons\\inv_misc_food_68",
+            source = "Sold by Malygen / Bale (Felwood)",
+        },
+        [46688] = {
+            icon = "Interface\\Icons\\inv_misc_food_65",
+            source = "Sold by Malygen / Bale (Felwood)",
+        },
+    },
+    [300] = {
+        [33279] = {
+            icon = "Interface\\Icons\\inv_misc_food_85_stegadonbite",
+            source = "Quest reward: \"Smooth as Butter\"",
+        },
+        [33284] = {
+            icon = "Interface\\Icons\\inv_misc_food_53",
+            source = "Sold by Cookie One-Eye (Hellfire Peninsula, Horde) / Sid Limbardi (Hellfire Peninsula, Alliance)",
+        },
+        [33290] = {
+            icon = "Interface\\Icons\\inv_misc_food_77",
+            source = "Sold by Gambarinka (Zangarmarsh, Horde) / Doba (Zangarmarsh, Alliance)",
+        },
+        [33291] = {
+            icon = "Interface\\Icons\\inv_misc_food_74",
+            source = "Sold by Zurai (Zangarmarsh, Horde) / Doba (Zangarmarsh, Alliance)",
+        },
+        [36210] = {
+            icon = "Interface\\Icons\\inv_misc_food_27",
+            source = "Sold by Mycah (Zangarmarsh)",
+        },
+        [43758] = {
+            icon = "Interface\\Icons\\inv_misc_food_91",
+            source = "Found in Crate of Meat, Barrel of Fish",
+        },
+        [43761] = {
+            icon = "Interface\\Icons\\inv_misc_fish_29",
+            source = "Found in Barrel of Fish",
+        },
+        [43772] = {
+            icon = "Interface\\Icons\\inv_misc_food_49",
+            source = "Found in Crate of Meat, Barrel of Fish",
+        },
+    },
+    [310] = {
+        [33285] = {
+            icon = "Interface\\Icons\\inv_misc_food_87_sporelingsnack",
+            source = "Sold by Mycah (Zangarmarsh)",
+        },
+        [33292] = {
+            icon = "Interface\\Icons\\inv_misc_food_79",
+            source = "Sold by Juno Dufrain (Zangarmarsh)",
+        },
+    },
+    [315] = {
+        [33286] = {
+            icon = "Interface\\Icons\\inv_misc_food_86_basilisk",
+            source = "Sold by Innkeeper Grilka (Terokkar Forest, Horde) / Supply Officer Mills (Terokkar Forest, Alliance)",
+        },
+    },
+    [320] = {
+        [33293] = {
+            icon = "Interface\\Icons\\inv_misc_food_78",
+            source = "Sold by Uriku (Nagrand, Alliance) / Nula the Butcher (Nagrand, Horde)",
+        },
+        [33294] = {
+            icon = "Interface\\Icons\\inv_misc_food_76",
+            source = "Sold by Uriku (Nagrand, Alliance) / Nula the Butcher (Nagrand, Horde)",
+        },
+    },
+    [325] = {
+        [33287] = {
+            icon = "Interface\\Icons\\inv_misc_food_60",
+            source = "Sold by Uriku (Nagrand, Alliance) / Nula the Butcher (Nagrand, Horde)",
+        },
+        [33288] = {
+            icon = "Interface\\Icons\\inv_misc_food_65",
+            source = "Sold by Innkeeper Grilka (Terokkar Forest, Horde) / Supply Officer Mills (Terokkar Forest, Alliance)",
+        },
+        [33289] = {
+            icon = "Interface\\Icons\\inv_misc_food_84_roastclefthoof",
+            source = "Sold by Uriku (Nagrand, Alliance) / Nula the Butcher (Nagrand, Horde)",
+        },
+        [33295] = {
+            icon = "Interface\\Icons\\inv_misc_fish_18",
+            source = "Sold by Rungor (Terokkar Forest, Horde) / Innkeeper Biribi (Terokkar Forest, Alliance)",
+        },
+        [43707] = {
+            icon = "Interface\\Icons\\inv_misc_food_63",
+            source = "Found in Barrel of Fish",
+        },
+        [43765] = {
+            icon = "Interface\\Icons\\inv_misc_food_84_roastclefthoof",
+            source = "Found in Crate of Meat",
+        },
+        [45022] = {
+            icon = "Interface\\Icons\\inv_drink_23",
+            source = "Sold by Penney Copperpinch (Orgrimmar, Horde) / Wulmort Jinglepocket (Ironforge, Alliance)",
+        },
+    },
+    [335] = {
+        [38867] = {
+            icon = "Interface\\Icons\\inv_misc_food_48",
+            source = "Sold by Xerintha Ravenoak (Blade's Edge Mountains, Horde) / Sassa Weldwell (Blade's Edge Mountains, Alliance)",
+        },
+        [38868] = {
+            icon = "Interface\\Icons\\inv_misc_food_88_ravagernuggets",
+            source = "Sold by Xerintha Ravenoak (Blade's Edge Mountains, Horde) / Sassa Weldwell (Blade's Edge Mountains, Alliance)",
+        },
+    },
+    [350] = {
+        [33296] = {
+            icon = "Interface\\Icons\\inv_misc_fish_16",
+            source = "Sold by Rungor (Terokkar Forest, Horde) / Innkeeper Biribi (Terokkar Forest, Alliance)",
+        },
+    },
+})
