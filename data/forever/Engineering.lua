bddcd9dbc78349a275677b888794a598a7ee7dcc
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-09-22 03:51:02 +0200

    ref

---
+++ data/forever/Engineering.lua
@@ -0,0 +1,59 @@
+TrainerSpellsBuiltin_Profession = TrainerSpellsBuiltin_Profession or {}
+TrainerSpellsBuiltin_Profession.Engineering = TrainerSpellsBuiltin_Profession.Engineering or {
+    [20] = {
+        [1230656] = {
+            icon = 133871,
+            cost = 95
+        },
+    },
+    [30] = {
+        [3922] = {
+            icon = 134068,
+            cost = 109
+        },
+        [3923] = {
+            icon = 133717,
+            cost = 124
+        },
+    },
+    [50] = {
+        [3924] = {
+            icon = 133025,
+            cost = 143
+        },
+        [3925] = {
+            icon = 135612,
+            cost = 143
+        },
+        [7430] = {
+            icon = 134520,
+            cost = 143
+        },
+    },
+    [60] = {
+        [3977] = {
+            icon = 134441,
+            cost = 190
+        },
+    },
+    [65] = {
+        [3926] = {
+            icon = 132997,
+            cost = 214
+        },
+    },
+    [75] = {
+        [3929] = {
+            icon = 133849,
+            cost = 238
+        },
+        [3930] = {
+            icon = 132384,
+            cost = 238
+        },
+        [3931] = {
+            icon = 133714,
+            cost = 238
+        },
+    },
+}
